5a3a294035a1bfe15219ce78055b338c85229060
Marcos Lopes <marcospaulolopesc@gmail.com>
2023-11-09 09:12:42 -0300

    aula 3

--- cifra/cifra.js
+++ cifra/cifra.js
@@ -1,13 +1,29 @@
-const senhaSecreta = "alura";
+const mensagemSecreta = "minhamensagemsecreta";
 
-function cifrarMensagem(mensagem, movimentos) {
+console.log(mensagemSecreta);
+
+function cifraMensagem(mensagem, movimentos) {
   const mensagemCifrada = mensagem.split("").map((caractere) => {
     const codigoCaractere = caractere.charCodeAt(0);
     return String.fromCharCode(codigoCaractere + movimentos);
   });
+
   return mensagemCifrada.join("");
 }
 
-const mensagemCifrada = cifrarMensagem(senhaSecreta, 4);
+const mensagemCifrada = cifraMensagem(mensagemSecreta, 3);
 
 console.log(mensagemCifrada);
+
+function decifraMensagem(mensagem, movimentos) {
+  const mensagemCifrada = mensagem.split("").map((caractere) => {
+    const codigoCaractere = caractere.charCodeAt(0);
+    return String.fromCharCode(codigoCaractere - movimentos);
+  });
+
+  return mensagemCifrada.join("");
+}
+
+const mensagemDecifrada = decifraMensagem(mensagemCifrada, 3);
+
+console.log(mensagemDecifrada);
